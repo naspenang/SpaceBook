9a91997efaebf5e1383669a3d0ec620994ade6ee
Nash Legion <naspenang@gmail.com>
2026-01-19 15:28:08 +0800

    gg

--- website/views/views_library.py
+++ website/views/views_library.py
@@ -6,20 +6,81 @@ from website.utils import save_library_image
 from django.shortcuts import get_object_or_404
 import os
 from django.conf import settings
+from website.models import Library, Campus, Branch
+
 
 
 
 
 def library_list(request):
-    libraries = Library.objects.all().order_by("library_name")
+    filters_visible = request.GET.get("filters", "1") != "0"
+    embed = request.GET.get("embed") == "1"
+    base_template = "website/embed_base.html" if embed else "website/base.html"
+
+
+    branch_code = request.GET.get("branch")
+    campus_code = request.GET.get("campus")
+
+    # If branch is not selected, ignore campus
+    if not branch_code:
+        campus_code = None
+
+    libraries = Library.objects.all()
+
+    # Branch filter (via Campus)
+    if branch_code:
+        campus_codes = (
+            Campus.objects
+            .filter(branch__code=branch_code)
+            .values_list("campus_code", flat=True)
+        )
+        libraries = libraries.filter(campus_code__in=campus_codes)
+
+    # Campus filter (more specific)
+    if campus_code:
+        libraries = libraries.filter(campus_code=campus_code)
+
+    # Branch dropdown (first)
+    branches = Branch.objects.all().order_by("name")
+
+    # Campus dropdown depends on branch
+    if branch_code:
+        campuses = (
+            Campus.objects
+            .filter(branch__code=branch_code)
+            .order_by("campus_name")
+        )
+    else:
+        campuses = Campus.objects.none()
+
+
+    # Build embed querystring cleanly
+    embed_params = request.GET.copy()
+    embed_params["filters"] = "0"
+    embed_params["embed"] = "1"
+
+    embed_querystring = embed_params.urlencode()
+
 
     return render(
         request,
         "website/library/library_list.html",
         {
-            "libraries": libraries
+            "libraries": libraries,
+            "branches": branches,
+            "campuses": campuses,
+            "selected_branch": branch_code,
+            "selected_campus": campus_code,
+            "filters_visible": filters_visible,
+            "base_template": base_template,
+            "embed_querystring": embed_querystring,
         }
     )
+
+
+
+
+
 
 
 def library_detail(request, library_code):
